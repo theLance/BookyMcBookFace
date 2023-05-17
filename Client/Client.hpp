fd0a86611c7271777f79db72b455be2f166f963a
Lance <lance.iliasics@gmail.com>
2023-05-17 15:00:57 +0200

    Add initial Client implementation + tests

---
+++ Client/Client.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "InputHandling/InputSanitizer.hpp"
+#include "Movies/TheaterAdmin.hpp"
+
+typedef std::unordered_map<std::string, std::shared_ptr<Theater>> TheaterMap;
+struct TheaterCapacity {
+    TheaterCapacity(const std::string& n, int c) : name(n), capacity(c) {}
+
+    std::string name;
+    int capacity;
+};
+typedef std::vector<TheaterCapacity> AvailableTheaters;
+
+class Client {
+    enum CheckShowMessage {
+        OK = 1,
+        NOT_FOUND,
+        CANCELLED
+    };
+
+    struct CheckShowResult {
+        CheckShowMessage msg;
+        TheaterMap::iterator it;
+    };
+public:
+
+    Client(TheaterAdmin& ta) : m_admin(ta) {}
+
+    MovieDb::Titles listAvailableMovies() const {
+        return m_admin.listCurrentMovies();
+    }
+
+    AvailableTheaters listTheatersPlayingMovie(const std::string& movie) {
+        m_theatersForMovie.clear();
+
+        AvailableTheaters result;
+        for(auto& res : m_admin.listTheatersPlayingMovie(movie)) {
+            if(res->freeCapacity()) {
+                result.emplace_back(res->name(), res->freeCapacity());
+                m_theatersForMovie.emplace(res->name(), res);
+            }
+        }
+        return result;
+    }
+
+    Seats listFreeSeatsForTheater(const std::string& theater) {
+        auto res = checkShowInCurrent(theater);
+        if(res.msg != CheckShowMessage::OK) {
+            return {};
+        }
+        auto result = res.it->second->freeSeats();
+        std::sort(result.begin(), result.end());
+        return result;
+    }
+
+    Booking book(const std::string& theater, const std::string& rawBookingString) {
+        auto input = convertAndSanitizeInput(rawBookingString);
+        auto res = checkShowInCurrent(theater);
+        if(res.msg != CheckShowMessage::OK) {
+            return {{},{},input};
+        }
+        return res.it->second->book(input);
+    }
+
+private:
+    CheckShowResult checkShowInCurrent(const std::string& theater) {
+        auto it = m_theatersForMovie.find(theater);
+        if(it == m_theatersForMovie.end()) {
+            return {CheckShowMessage::NOT_FOUND, it};
+        }
+        if(it->second.use_count() == 1) {
+            m_theatersForMovie.erase(it);
+            return {CheckShowMessage::CANCELLED, it};
+        }
+        return {CheckShowMessage::OK, it};
+    }
+
+    TheaterAdmin& m_admin;
+
+    TheaterMap m_theatersForMovie;
+};
---
+++ Tests/TestClient.cpp
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "Movies/TheaterAdmin.hpp"
+#include "Client/Client.hpp"
+
+struct ClientFixture : ::testing::Test {
+    const std::string MOVIE_1 = "cica";
+    const std::string THEATER_NAME = "cicaplex";
+    const std::string MOVIE_2 = "cica2 - the sequel";
+    const std::string OTHER_THEATER_NAME = "cicaverse";
+
+    ClientFixture() : sut(admin) {
+        admin.registerTheaterShowingMovie(THEATER_NAME, MOVIE_1);
+    }
+
+    TheaterAdmin admin;
+    Client sut;
+};
+
+TEST_F(ClientFixture, list_movies) {
+    ASSERT_EQ(1, sut.listAvailableMovies().size());
+    ASSERT_EQ(MOVIE_1, sut.listAvailableMovies()[0]);
+}
+
+TEST_F(ClientFixture, list_theaters_for_movies) {
+    ASSERT_EQ(1, sut.listTheatersPlayingMovie(MOVIE_1).size());
+    ASSERT_EQ(THEATER_NAME, sut.listTheatersPlayingMovie(MOVIE_1)[0].name);
+}
+
+TEST_F(ClientFixture, list_theaters_for_movies__not_avialable) {
+    ASSERT_EQ(0, sut.listTheatersPlayingMovie(MOVIE_2).size());
+}
+
+std::vector<std::string> fullCapacity = {"a1", "a2", "a3", "a4", "a5",
+                                         "b1", "b2", "b3", "b4", "b5",
+                                         "c1", "c2", "c3", "c4", "c5",
+                                         "d1", "d2", "d3", "d4", "d5"};
+
+TEST_F(ClientFixture, get_free_seats__initially_all_free) {
+    sut.listTheatersPlayingMovie(MOVIE_1); // necessary to load theater
+    ASSERT_EQ(fullCapacity, sut.listFreeSeatsForTheater(THEATER_NAME));
+}
+
+TEST_F(ClientFixture, cant_find_seats_if_theater_not_playing_movie) {
+    sut.listTheatersPlayingMovie(MOVIE_1); // necessary to load theater
+    ASSERT_EQ(Seats{}, sut.listFreeSeatsForTheater(OTHER_THEATER_NAME));
+}
+
+TEST_F(ClientFixture, get_free_seats__booking_makes_seats_not_free) {
+    sut.listTheatersPlayingMovie(MOVIE_1); // necessary to load theater
+    EXPECT_EQ(fullCapacity, sut.listFreeSeatsForTheater(THEATER_NAME));
+
+    auto result = sut.book(THEATER_NAME, "a1,a2,a3");
+    ASSERT_EQ(3, result.success.size());
+    ASSERT_EQ(0, result.taken.size());
+    ASSERT_EQ(0, result.invalid.size());
+    std::vector<std::string> expected = {                  "a4", "a5",
+                                         "b1", "b2", "b3", "b4", "b5",
+                                         "c1", "c2", "c3", "c4", "c5",
+                                         "d1", "d2", "d3", "d4", "d5"};
+    ASSERT_EQ(expected, sut.listFreeSeatsForTheater(THEATER_NAME));
+}
+
